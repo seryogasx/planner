7ce98566600b850cad0ed05fda3aa8989b538b62
seryogasx <sergio.petrovx@yandex.ru>
2020-08-02 11:17:27 +0300

    Initial Commit

---
+++ planner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  planner
+//
+//  Created by Сергей Петров on 02.08.2020.
+//  Copyright © 2020 SergioPetrovx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
