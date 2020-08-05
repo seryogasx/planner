1aeeace4c913a80e4ec111010018d5004a9705aa
seryogasx <sergio.petrovx@yandex.ru>
2020-08-05 18:52:43 +0300

    add CoreData save and load

--- planner/ViewConrollers/ViewController.swift
+++ planner/ViewConrollers/ViewController.swift
@@ -10,16 +10,43 @@ import UIKit
 import CoreData
 
 
+func loadFromStorage() -> [NSManagedObject] {
+    guard let appDelegate =
+      UIApplication.shared.delegate as? AppDelegate else {
+        return []
+    }
+    
+    let managedContext =
+      appDelegate.persistentContainer.viewContext
+    
+    //2
+    let fetchRequest =
+      NSFetchRequest<NSManagedObject>(entityName: "NoteData")
+    
+    var notes: [NSManagedObject] = []
+    do {
+        notes = try managedContext.fetch(fetchRequest)
+    } catch let error as NSError {
+        print("Some problems with load data! -> \(error) ; \(error.userInfo) ; \(error.description)")
+    }
+    return notes
+}
+
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var settingsButton: UIBarButtonItem!
     @IBOutlet weak var notesTable: UITableView!
     
-    var notesData = DataStorage.shared.loadAllNotes()
-//    var notesData = [NSManagedObject]()
+    var notesData: [NSManagedObject] = []
+    
+    @IBAction func NewNoteButtonClicked(_ sender: UIBarButtonItem) {
+        save(newNote: Note(description: "keks", state: true))
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.notesData = loadFromStorage()
     }
 }
 
@@ -35,18 +62,46 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
     }
        
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let note = notesData[indexPath.row]
+        let text = note.value(forKey: "text") as! String
+        let state = note.value(forKey: "state") as! Bool
+        let finishDate = note.value(forKey: "finishDate") as? Date
+        
         let cell: NoteCell = self.notesTable.dequeueReusableCell(withIdentifier: cellReuseIdentifier, for: indexPath) as! NoteCell
         
-        cell.descriptionTextField.text = self.notesData[indexPath.row].description
-        cell.note = notesData[indexPath.row]
+        cell.descriptionTextField.text = text
+        cell.note = Note(description: text, state: state, finishDate: finishDate)
         cell.setButtonImage()
         
-        guard let finishDate = self.notesData[indexPath.row].finishDate else {
+        guard finishDate != nil else {
             cell.finishDateLabel.isHidden = true
             return cell
         }
         
-        cell.finishDateLabel.text = finishDate.noteDate
+        cell.finishDateLabel.text = finishDate?.noteDate
         return cell
+    }
+}
+
+
+func save(newNote: Note) {
+    guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+        return
+    }
+    
+    let managedContext = appDelegate.persistentContainer.viewContext
+    
+    let entity = NSEntityDescription.entity(forEntityName: "NoteData", in: managedContext)!
+    
+    let note = NSManagedObject(entity: entity, insertInto: managedContext)
+    
+    note.setValue(newNote.description, forKey: "text")
+    note.setValue(newNote.finishDate, forKey: "finishDate")
+    note.setValue(newNote.state, forKey: "state")
+    
+    do {
+        try managedContext.save()
+    } catch let error as NSError {
+        print("Some problems with save data! -> \(error) ; \(error.userInfo) ; \(error.description)")
     }
 }
